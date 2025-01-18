40bf65cbcb49028780b364a92505ac3612ab9b0f
camila-valderrama <camival00@gmail.com>
2025-01-17 22:17:15 -0300

    Ajustando cambios

--- app.mjs
+++ app.mjs
@@ -1,6 +1,6 @@
 import express from 'express';
-import { connectDB } from './config/dbConfig.mjs';
-import router from './routes/superHeroRoutes.mjs';
+import { connectDB } from './src/config/dbConfig.mjs';
+import router from './src/routes/superHeroRoutes.mjs';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import expressLayouts from 'express-ejs-layouts';
@@ -16,7 +16,7 @@ const __dirname = dirname(__filename);
 app.use(express.static(join(__dirname, 'public')));
 
 // Configurar directorio de vistas y motor de plantillas
-app.set('views', join(__dirname, 'views'));
+app.set('views', join(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
 //Configurar express ejs Layout
